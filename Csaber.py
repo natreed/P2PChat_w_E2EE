705d576361e9bfdbd69c992bb63add66369d311b
natreed <natreed@pdx.edu>
2015-12-01 15:10:17 -0800

    simplified rc4

--- Csaber.py
+++ Csaber.py
@@ -7,28 +7,6 @@ from os import urandom
 password = str.encode('password')
 REPS = 20
 
-#Function for encryption same as
-def rc42(input, key):
-    input = str.encode(input)
-    input = bytearray(input)
-    cipher = bytearray(len(input))
-    i, j, state, keystream = 0, 0, list(range(256)), bytearray(list(range(len(input))))
-
-    for r in range(REPS):
-        for i in range(256):
-            j = (j + state[i] + key[i % len(key)]) % 256
-            state[i], state[j] = state[j], state[i]
-
-    j = 0
-    for i in range(0, len(input)):
-        x = (i + 1) % 256
-        j = (j + state[x]) % 256
-        state[x], state[j] = state[j], state[x]
-        keystream[i] = state[(state[x] + state[j]) % 256]
-        cipher[i] = ((keystream[i]) ^ input[i])
-
-    return cipher
-
 def rc43(input, key):
     cipher = bytearray(len(input))
     i, j, state, keystream = 0, 0, list(range(256)), bytearray(list(range(len(input))))
@@ -50,10 +28,11 @@ def rc43(input, key):
 
 
 def encrypt(message, password):
-	iv = urandom(10)
-	password += iv
-	encryptedMessage = rc42(message, password)
-	return iv + encryptedMessage  # concatenate iv and encrypted message
+    message = str.encode(message)
+    iv = urandom(10)
+    password += iv
+    encryptedMessage = rc43(message, password)
+    return iv + encryptedMessage  # concatenate iv and encrypted message
 
 
 def decrypt(message, password):
